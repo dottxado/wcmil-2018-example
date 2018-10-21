c3cf6fb7dcb3cd54a7f06bfc1d7340f11c762ed2
Erika Gili <erika.gili@crispybacon.it>
2018-10-21 16:25:54 +0200

    Scaffolded block with wp-cli (wp scaffold block block-01 --plugin=wcmil-2018-example)

---
+++ blocks/block-01.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * Functions to register client-side assets (scripts and stylesheets) for the
+ * Gutenberg block.
+ *
+ * @package wcmil-2018-example
+ */
+
+/**
+ * Registers all block assets so that they can be enqueued through Gutenberg in
+ * the corresponding context.
+ *
+ * @see https://wordpress.org/gutenberg/handbook/blocks/writing-your-first-block-type/#enqueuing-block-scripts
+ */
+function block_01_block_init() {
+	// Skip block registration if Gutenberg is not enabled/merged.
+	if ( ! function_exists( 'register_block_type' ) ) {
+		return;
+	}
+	$dir = dirname( __FILE__ );
+
+	$index_js = 'block-01/index.js';
+	wp_register_script(
+		'block-01-block-editor',
+		plugins_url( $index_js, __FILE__ ),
+		array(
+			'wp-blocks',
+			'wp-i18n',
+			'wp-element',
+		),
+		filemtime( "$dir/$index_js" )
+	);
+
+	$editor_css = 'block-01/editor.css';
+	wp_register_style(
+		'block-01-block-editor',
+		plugins_url( $editor_css, __FILE__ ),
+		array(),
+		filemtime( "$dir/$editor_css" )
+	);
+
+	$style_css = 'block-01/style.css';
+	wp_register_style(
+		'block-01-block',
+		plugins_url( $style_css, __FILE__ ),
+		array(),
+		filemtime( "$dir/$style_css" )
+	);
+
+	register_block_type( 'wcmil-2018-example/block-01', array(
+		'editor_script' => 'block-01-block-editor',
+		'editor_style'  => 'block-01-block-editor',
+		'style'         => 'block-01-block',
+	) );
+}
+add_action( 'init', 'block_01_block_init' );
