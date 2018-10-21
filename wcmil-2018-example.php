0404de4d8f9f55d738857a69b50194c21928b17f
Erika Gili <erika.gili@crispybacon.it>
2018-10-21 16:14:28 +0200

    Plugin creation

---
+++ wcmil-2018-example.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+Plugin Name: Wcmil 2018 Example
+Plugin URI: https://2018.milano.wordcamp.org/session/guida-pratica-al-primo-approccio-con-gutenberg-per-programmatori-php/
+Description: Gutenberg example for WCMIL 2018
+Version: 1.0
+Author: @ingerikagili
+Author URI: http://www.crispybacon.it
+License: GPL2
+*/
+
+// Exit if accessed directly.
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
